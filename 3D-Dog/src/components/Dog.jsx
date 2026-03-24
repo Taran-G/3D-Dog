29635cc650f87d6065b050b63d3bda8d00cd4079
Taran Grewal <tarangrewal008@gmail.com>
2026-03-24 10:08:22 -0700

    used direction light so that the object is visible and then to make the object in position we changed the y axis ,and the x axis,we also rotates the object on y axis point finally the object was too far from camera so we decreased the distance from the camera

--- 3D-Dog/src/components/Dog.jsx
+++ 3D-Dog/src/components/Dog.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import {useThree} from '@react-three/fiber'
-import {OrbitControls, orbitCOntrols,useGLTF} from '@react-three/drei'
+import {useGLTF} from '@react-three/drei'
+import {OrbitControls} from '@react-three/drei'
 
 const Dog = () => {
-     useThree(({Camera,Scene,GL})=>{
+     useThree(({camera,scene,GL})=>{
+        camera.position.z = 0.55;
 
-     })
+             })
      const model = useGLTF('/models/dog.drc.glb')
   return (
-        <primitive object = {model.scene} />
+    <>
+        <primitive object = {model.scene} position ={[0.26, -0.5, 0]} rotation = {[0,Math.PI/3.9,0]}/>
+        <directionalLight position={[0, 0, 2]} color={0xFFFFFF} intensity ={10}/>
+        <OrbitControls />
+        </>
         
   )
 }
