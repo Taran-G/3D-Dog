f95b58dabda61666dd72e5962131deaab186b34f
Taran Grewal <tarangrewal008@gmail.com>
2026-03-24 07:06:44 -0700

    we imported orbit control ,bwfore the orientation was fixed that is we can only seee one side,by orbit control well we able to change the orientation of the object by a cursor and we have also imported useGltf by which well we able to add our 3d model in the scene,we have made a model which have a scene which holds the 3d object so in primitive we are using model.scene,primitive helps u to render this on the screen

--- 3D-Dog/src/components/Dog.jsx
+++ 3D-Dog/src/components/Dog.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import {useThree} from '@react-three/fiber'
+import {OrbitControls, orbitCOntrols,useGLTF} from '@react-three/drei'
 
 const Dog = () => {
      useThree(({Camera,Scene,GL})=>{
 
      })
+     const model = useGLTF('/models/dog.drc.glb')
   return (
-        <mesh>
-            <meshBasicMaterial color = "green"/>
-            <boxGeometry args ={[1,1,1]}/>
-        </mesh>
-    
+        <primitive object = {model.scene} />
+        
   )
 }
 
